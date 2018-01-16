4aa302e78c8455bb6f3ede06afced9efe08398b4
Allan Liebold <allan.liebold@gmail.com>
2018-01-16 02:36:06 -0800

    player input flips the player character sprite left or right

--- PlayerController.cs
+++ PlayerController.cs
@@ -9,4 +9,18 @@ public class PlayerController : MonoBehaviour {
   void Start() {
     boxCollider = GetComponent<BoxCollider2D>();
   }
+
+  void FixedUpdate() {
+    float x = Input.GetAxisRaw("Horizontal");
+    float y = Input.GetAxisRaw("Vertical");
+
+    // moveDelta is reset every frame
+    moveDelta = new Vector3(x, y, 0);
+
+    // Change player sprite facing direction, default if right, change x to -1 if left
+    if(moveDelta.x > 0)
+      transform.localScale = Vector3.one;
+    else if(moveDelta.x < 0)
+      transform.localScale = new Vector3(-1, 1, 1);
+  }
 }
