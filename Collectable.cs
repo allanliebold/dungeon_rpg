9e0a46a45c43f98afabe10db500bfa0cd95ec428
Allan Liebold <allan.liebold@gmail.com>
2018-01-18 23:43:52 -0800

    collectable script overrides the inherited on collide function to call the on collect script whenever it collides with the player object

--- Collectable.cs
+++ Collectable.cs
@@ -5,7 +5,13 @@ using System.Collections.Generic;
 public class Collectable : CollisionObject {
   protected bool collected;
 
+  protected override void OnCollide(Collider2D collider) {
+    if(collider.name == "Player") {
+      OnCollect();
+    }
+  }
+
   protected virtual void OnCollect() {
-    
+
   }
 }
