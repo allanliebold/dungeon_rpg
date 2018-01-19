5696959c560a702033c855e16133bd057bf8df49
Allan Liebold <allan.liebold@gmail.com>
2018-01-18 23:55:22 -0800

    added gold amount variable to chest script with a default value of 5

--- Chest.cs
+++ Chest.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 public class Chest : Collectable {
 
   public Sprite emptyChest;
-  
+  public int goldAmount = 5;
+
   protected override void OnCollect(Collider2D collider) {
     if(!collected) {
       collected = true;
