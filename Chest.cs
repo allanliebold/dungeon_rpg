3087c2409858b0a2f33356627159270c4157c7b8
Allan Liebold <allan.liebold@gmail.com>
2018-01-18 23:25:38 -0800

    created script for chest object that inherits from the collider object class

---
+++ Chest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chest : CollisionObject {
+  protected override void OnCollide(Collider2D collider) {
+    base.OnCollide(collider);
+  }
+}
