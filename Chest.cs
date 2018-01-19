3913e0b6d32c37cb983b88a5822f76b26c5acf87
Allan Liebold <allan.liebold@gmail.com>
2018-01-18 23:54:39 -0800

    added sprite object to chest script to change the object's sprite after it has been collected

--- Chest.cs
+++ Chest.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Chest : Collectable {
+
+  public Sprite emptyChest;
+  
   protected override void OnCollect(Collider2D collider) {
     if(!collected) {
       collected = true;
