fa22b90edda6d22d7b89108107c2803d024e01bc
Allan Liebold <allan.liebold@gmail.com>
2018-02-22 06:15:40 -0800

    created if condition for checking if the collided object is the player

--- Portal.cs
+++ Portal.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 public class Portal : ColliderObject
 {
     public string[] sceneNames;
-    
+
+    protected override void OnCollide(Collider2D coll) {
+      if (coll.name == "Player" )
+      {
+        //Teleport the Player
+        string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+      }
+    }
+
 }
