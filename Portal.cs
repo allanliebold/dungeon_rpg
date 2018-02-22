fa46ceeccd6d2b392a89806087e0e3ef3381118d
Allan Liebold <allan.liebold@gmail.com>
2018-02-22 06:14:13 -0800

    created array to hold names of scenes to teleport player to

--- Portal.cs
+++ Portal.cs
@@ -2,4 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : Collidable
+public class Portal : ColliderObject
+{
+    public string[] sceneNames;
+    
+}
